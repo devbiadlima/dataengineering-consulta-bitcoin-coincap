dc5ffc4da1d13efda07ef5a61153370be067aa93
Bia <biadlima.dev@outlook.com>
2023-10-13 15:51:47 -0300

    envio de e-mail

---
+++ server.js
@@ -0,0 +1,40 @@
+const option = {
+    method: "GET",
+    mode: 'cors',
+    cache: 'default'
+}
+
+fetch('https://api.coincap.io/v2/assets/bitcoin', option)
+    .then(response => {response.json()
+        .then( data => console.log(data))})
+        .catch(e => console.log('Deu Erro: '+ e,message));
+
+        
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.office365.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: "alertabitcoincap@outlook.com",
+        pass:"Matrix1324##"
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
+});
+
+async function run(){
+    const mailSent = await transporter.sendMail({
+        text: 'Alerta cotação Bitcoin',
+        subject: 'Alerta cotação Bitcoin',
+        from: 'alertabitcoincap@outlook.com',
+        to: ['biadlima@live.com']
+    });
+
+    console.log(mailSent);
+}
+
+run();
